aa1bdf172c45180abe9ed6d3ecc465d4c4a8fbcd
Johnathan Crook-Davies <johnathanmdavies@gmail.com>
2020-05-04 22:22:45 +0100

    Added stocks to indices request.

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -1,28 +1,32 @@
 class IndicesController < ApplicationController
     
-    def show
-        # Returns single index
-        
-        if ind = Index.find_by(symbol: index_params[:id])
-            render json: ind.stocks
-        else
-            raise ActionController::RoutingError.new('Not Found')
-        end
+  def show
+    # Returns single index
+
+    if ind = Index.find_by(symbol: index_params[:id])
+      render json: ind.stocks
+    else
+      raise ActionController::RoutingError.new('Not Found')
     end
-    
-    def index
-        # Returns all indices
-        
-        if inds = Index.all
-            render json: inds
-        else
-            raise ActionController::RoutingError.new('Not Found')
-        end
+  end
+
+  def index
+    # Returns all indices
+
+    if inds = Index.all
+      result = []
+      inds.each do | i |
+        result << {index: i, stocks: i.stocks.all}
+      end
+      render json: result
+    else
+      raise ActionController::RoutingError.new('Not Found')
     end
-    
-    private
-        def index_params
-            params.permit(:id)
-        end
+  end
+
+  private
+    def index_params
+      params.permit(:id)
+    end
     
 end
--- spec/requests/indices_request_spec.rb
+++ spec/requests/indices_request_spec.rb
@@ -2,42 +2,49 @@ require 'rails_helper'
 
 RSpec.describe "Indices", type: :request do
     
-    let(:ind) { build(:index) }
-    
-    before do
-        ind.save
-        # Stub methods in module AlphaVantage
-        allow_any_instance_of(Index).to receive(:get_index_price).with(anything()) { returned_get_index_price }
-    end
+  let(:ind) { build(:index) }
 
-    describe 'GET show' do
-        before do
-            get "/indices/#{ind.symbol}"
-        end
-        it "returns response 200: OK" do
-            expect(response.status).to eq(200)
-        end
-        it "returns a json" do
-            expect(response.content_type).to eq("application/json; charset=utf-8")
-        end
-        it "responds with constituent stocks" do
-            expect(JSON.parse(response.body_parts[0])).to eq(ind.stocks.to_a)
-        end
+  before do
+    ind.save
+    # Stub methods in module AlphaVantage
+    allow_any_instance_of(Index).to receive(:get_index_price).with(anything()) { returned_get_index_price }
+  end
+
+  describe 'GET show' do
+    before do
+      get "/indices/#{ind.symbol}"
     end
-    
-    describe 'GET index' do
-        before do
-            get "/indices"
-        end
-        it "returns response 200: OK" do
-            expect(response.status).to eq(200)
-        end
-        it "returns a json" do
-            expect(response.content_type).to eq("application/json; charset=utf-8")
-        end
-        it "responds with object" do
-            expect(JSON.parse(response.body_parts[0])[0]).to include(ind.attributes.slice('id', 'symbol'))
-        end
+    it "returns response 200: OK" do
+      expect(response.status).to eq(200)
     end
+    it "returns a json" do
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+    it "responds with constituent stocks" do
+      expect(JSON.parse(response.body_parts[0])).to eq(ind.stocks.to_a)
+    end
+  end
+
+  describe 'GET index' do
+    before do
+      get "/indices"
+    end
+    it "returns response 200: OK" do
+      expect(response.status).to eq(200)
+    end
+    it "returns a json" do
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+    it "responds with object" do
+      expect(
+        JSON.parse(response.body_parts[0])[0]["index"]
+      ).to include("symbol" => ind.symbol)
+    end
+    it "responds with constituent stocks" do
+      expect(
+        JSON.parse(response.body_parts[0])[0]["stocks"].to_json
+      ).to include(ind.stocks.to_json)
+    end
+  end
 
 end
